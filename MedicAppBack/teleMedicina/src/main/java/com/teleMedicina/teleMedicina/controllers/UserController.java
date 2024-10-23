4bcd6340d891b0aca81d60d4d1bd2b28a8274146
Ronald <ronaldavid1908@hotmail.com>
2024-10-22 21:57:23 -0500

    Se crea el crud Usuario, conexion BD, pruebaspostman

---
+++ MedicAppBack/teleMedicina/src/main/java/com/teleMedicina/teleMedicina/controllers/UserController.java
@@ -0,0 +1,49 @@
+package com.teleMedicina.teleMedicina.controllers;
+
+import com.teleMedicina.teleMedicina.models.UserModel;
+import com.teleMedicina.teleMedicina.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuario")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+// Todos los usuarios
+    @GetMapping
+    public ResponseEntity<List<UserModel>> getAllUsers() {
+        List<UserModel> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+//Un usuario
+    @GetMapping("/{id}")
+    public ResponseEntity<UserModel> getUserById(@PathVariable Integer id) {
+        UserModel user = userService.getUserById(id);
+        return user != null ? new ResponseEntity<>(user, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+//Agregar usuario
+    @PostMapping
+    public ResponseEntity<UserModel> createUser(@Valid @RequestBody UserModel user) {
+        UserModel savedUser = userService.saveUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+//Editar usuario
+    @PutMapping("/{id}")
+    public ResponseEntity<UserModel> updateUser(@PathVariable Integer id, @Valid @RequestBody UserModel user) {
+        UserModel updatedUser = userService.updateUser(id, user);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+//Borrar usuario
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
+        userService.deleteUser(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ MedicAppBack/teleMedicina/src/main/java/com/teleMedicina/teleMedicina/models/UserModel.java
@@ -0,0 +1,42 @@
+package com.teleMedicina.teleMedicina.models;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuario")
+@Getter
+@Setter
+
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUsuario;
+
+    @NotEmpty(message = "El nombre no puede estar vacío")
+    private String nombre;
+
+    @NotEmpty(message = "El apellido no puede estar vacío")
+    private String apellido;
+
+    @NotEmpty(message = "El correo electrónico no puede estar vacío")
+    @Column(unique = true)
+    private String correoElectronico;
+
+    @NotEmpty(message = "La contraseña no puede estar vacía")
+    private String contraseña;
+
+    @Enumerated(EnumType.STRING)
+    private rol rol;
+}
